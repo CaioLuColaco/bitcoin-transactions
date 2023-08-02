851de034b7f1fc1b0f486378daef39524c390cfd
caio lucena <caio.lcolaco@gmail.com>
2023-08-02 00:54:34 -0300

    feat: Created loggers, schemas and DB connection structure

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+
+	"github.com/CaioLuColaco/bitcoin-transactions/config"
 	"github.com/CaioLuColaco/bitcoin-transactions/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return 
+	}
+
 	router.Initialize()
 }
