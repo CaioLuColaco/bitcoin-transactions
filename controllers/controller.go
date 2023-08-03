91ce156d2d28e9a8e22568e438b6138c45ccdb96
caio lucena <caio.lcolaco@gmail.com>
2023-08-03 08:21:02 -0300

    feat: Split the create transaction controller

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,10 +1,6 @@
 package controllers
 
 import (
-	"bytes"
-	"encoding/json"
-	"net/http"
-
 	"github.com/CaioLuColaco/bitcoin-transactions/database"
 	"github.com/CaioLuColaco/bitcoin-transactions/models"
 	"github.com/gin-gonic/gin"
@@ -18,60 +14,6 @@ func ShowAllTransactions(c *gin.Context) {
 
 func ShowOneTransaction(c *gin.Context) {
 
-}
-
-func CreateTransaction(c *gin.Context) {
-	hash := c.Params.ByName("hash")
-
-	destURL := "https://mainnet.infura.io/v3/4355c5add5574242b01ce888c231f7bf"
-	requestBody := map[string]interface{}{
-		"jsonrpc": "2.0",
-		"method":  "eth_getTransactionByHash",
-		"params":  []interface{}{hash},
-		"id":      1,
-	}
-
-	jsonBody, err := json.Marshal(requestBody)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao serializar JSON"})
-		return
-	}
-
-	resp, err := http.Post(destURL, "application/json", bytes.NewBuffer(jsonBody))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao fazer a requisição"})
-		return
-	}
-	defer resp.Body.Close()
-
-	var responseData map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&responseData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao decodificar a resposta JSON"})
-		return
-	}
-
-	var transaction models.Transaction
-	transaction.BlockHash = responseData["result"].(map[string]interface{})["blockHash"].(string)
-	transaction.BlockNumber = responseData["result"].(map[string]interface{})["blockNumber"].(string)
-	transaction.ChainId = responseData["result"].(map[string]interface{})["chainId"].(string)
-	transaction.From = responseData["result"].(map[string]interface{})["from"].(string)
-	transaction.Gas = responseData["result"].(map[string]interface{})["gas"].(string)
-	transaction.GasPrice = responseData["result"].(map[string]interface{})["gasPrice"].(string)
-	transaction.Hash = responseData["result"].(map[string]interface{})["hash"].(string)
-	transaction.MaxFeePerGas = responseData["result"].(map[string]interface{})["maxFeePerGas"].(string)
-	transaction.MaxPriorityFeePerGas = responseData["result"].(map[string]interface{})["maxPriorityFeePerGas"].(string)
-	transaction.Nonce = responseData["result"].(map[string]interface{})["nonce"].(string)
-	transaction.R = responseData["result"].(map[string]interface{})["r"].(string)
-	transaction.To = responseData["result"].(map[string]interface{})["to"].(string)
-	transaction.TransactionIndex = responseData["result"].(map[string]interface{})["transactionIndex"].(string)
-	transaction.Tp = responseData["result"].(map[string]interface{})["type"].(string)
-	transaction.V = responseData["result"].(map[string]interface{})["v"].(string)
-	transaction.Value = responseData["result"].(map[string]interface{})["value"].(string)
-
-	database.DB.Create(&transaction)
-
-	c.JSON(http.StatusOK, transaction)
 }
 
 func UpdateTransaction(c *gin.Context) {
---
+++ controllers/createTransaction.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/CaioLuColaco/bitcoin-transactions/database"
+	"github.com/CaioLuColaco/bitcoin-transactions/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTransaction(c *gin.Context) {
+	hash := c.Params.ByName("hash")
+
+	destURL := "https://mainnet.infura.io/v3/4355c5add5574242b01ce888c231f7bf"
+	requestBody := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"method":  "eth_getTransactionByHash",
+		"params":  []interface{}{hash},
+		"id":      1,
+	}
+
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao serializar JSON"})
+		return
+	}
+
+	resp, err := http.Post(destURL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao fazer a requisição"})
+		return
+	}
+	defer resp.Body.Close()
+
+	var responseData map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&responseData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao decodificar a resposta JSON"})
+		return
+	}
+
+	var transaction models.Transaction
+	responseResult, _ := responseData["result"].(map[string]interface{})
+
+	transaction.BlockHash = getStringFromResponse(responseResult, "blockHash")
+	transaction.BlockNumber = getStringFromResponse(responseResult, "blockNumber")
+	transaction.ChainId = getStringFromResponse(responseResult, "chainId")
+	transaction.From = getStringFromResponse(responseResult, "from")
+	transaction.Gas = getStringFromResponse(responseResult, "gas")
+	transaction.GasPrice = getStringFromResponse(responseResult, "gasPrice")
+	transaction.Hash = getStringFromResponse(responseResult, "hash")
+	transaction.MaxFeePerGas = getStringFromResponse(responseResult, "maxFeePerGas")
+	transaction.MaxPriorityFeePerGas = getStringFromResponse(responseResult, "maxPriorityFeePerGas")
+	transaction.Nonce = getStringFromResponse(responseResult, "nonce")
+	transaction.R = getStringFromResponse(responseResult, "r")
+	transaction.To = getStringFromResponse(responseResult, "to")
+	transaction.TransactionIndex = getStringFromResponse(responseResult, "transactionIndex")
+	transaction.Tp = getStringFromResponse(responseResult, "type")
+	transaction.V = getStringFromResponse(responseResult, "v")
+	transaction.Value = getStringFromResponse(responseResult, "value")
+	
+	database.DB.Create(&transaction)
+
+	c.JSON(http.StatusOK, transaction)
+}
+
+func getStringFromResponse(data map[string]interface{}, key string) string {
+    value, ok := data[key]
+    if !ok || value == nil {
+        return ""
+    }
+    strValue, _ := value.(string)
+    return strValue
+}
